1382d29a1e8d40d1246be79e4cf90bb67f70649d
Vinícius Costa <vinicius.costa@lmdc.uff.br>
2022-01-16 11:30:39 -0300

    teste

--- src/App.js
+++ src/App.js
@@ -1,25 +1,104 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import CytoscapeComponent from 'react-cytoscapejs';
+import {consoleLogTest} from './jsonSampleParser.js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+export default function App() {
+  consoleLogTest();
+  const [width, setWith] = useState("100%");
+  const [height, setHeight] = useState("400px");
+//Adding elements here
+  const [graphData, setGraphData] = useState([
+    //Node format
+    { data: { id: '1', label: 'Node 1' }},
+    { data: { id: '2', label: 'Node 2' }},
+    { data: { id: '3', label: 'Node 3' }},
+    { data: { id: '4', label: 'Node 4' }},
+    { data: { id: '5', label: 'Node 5' }},
+    { data: { id: '6', label: 'Node 6' }},
+    { data: { id: '7', label: 'Node 7' }},
+    { data: { id: '8', label: 'Node 8' }},
+//Edge format
+    { data: { source: '1', target: '2', label: 'Edge from 1 to 2'}},
+    { data: { source: '1', target: '3', label: 'Edge from 1 to 3'}},
+    { data: { source: '4', target: '5', label: 'Edge from 4 to 5'}},
+    { data: { source: '6', target: '8', label: 'Edge from 6 to 8'}},
+]);
+return (
+    <>
+      <div>
+        <h1>My Cytoscape example</h1>
+        <div
+          style={{
+            border: "1px solid",
+            backgroundColor: "#f5f6fe"
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <CytoscapeComponent
+            elements={graphData}
+            style={{ width: width, height: height }}
+            //adding a layout
+            layout={{
+              name: 'breadthfirst',
+              fit: true,
+              directed: true,
+              padding: 50,
+              animate: true,
+              animationDuration: 1000,
+              avoidOverlap: true,
+              nodeDimensionsIncludeLabels: false
+            }}
+            //adding style sheet
+            stylesheet={[
+              {
+                selector: "node",
+                style: {
+                  backgroundColor: "#555",
+                  width: 60,
+                  height: 60,
+                  label: "data(label)",
+                  "text-valign": "center",
+                  "text-halign": "center",
+                  "text-outline-color": "#555",
+                  "text-outline-width": "2px",
+                  "overlay-padding": "6px",
+                  "z-index": "10"
+                }
+              },
+              {
+                selector: "node:selected",
+                style: {
+                  "border-width": "6px",
+                  "border-color": "#AAD8FF",
+                  "border-opacity": "0.5",
+                  "background-color": "#77828C",
+                  "text-outline-color": "#77828C"
+                }
+              },
+              {
+                selector: "label",
+                style: {
+                  color: "white",
+                  width: 30,
+                  height: 30,
+                  fontSize: 30
+                  // shape: "rectangle"
+                }
+              },
+              {
+                selector: "edge",
+                style: {
+                  width: 3,
+                  // "line-color": "#6774cb",
+                  "line-color": "#AAD8FF",
+                  "target-arrow-color": "#6774cb",
+                  "target-arrow-shape": "triangle",
+                  "curve-style": "bezier"
+                }
+              }
+            ]}
+         />
+        </div>
+      </div>
+    </>
   );
 }
-
-export default App;
---
+++ src/jsonSampleParser.js
@@ -0,0 +1,3 @@
+export const consoleLogTest = () =>{
+    console.log("aaa")
+}
